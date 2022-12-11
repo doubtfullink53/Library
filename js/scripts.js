7a68966418c7cc8c9dca700ea5796df29195b85d
Robby <doubtfullink53@gmail.com>
2022-12-11 16:36:45 -0500

    add push title to array and add book forms

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,34 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
-    myLibrary.push
+function addBookToLibrary(title) {
+    myLibrary.push(title)
+    
 }
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
-console.log(theHobbit.info())
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+// console.log(addBookToLibrary(theHobbit))
 
-// modal popup for input of book 
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+function logSubmit(event) {
+  log.textContent = `complete`;
+  
+  let newBook = new Book(title.value , author.value, pages.value, read[0].checked)
+  addBookToLibrary(newBook)
+  // console.log(newBook)
+  console.log(myLibrary[0])
+  event.preventDefault();
+}
+
+console.log(myLibrary)
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementsByClassName('read');
+
+
+// console.log(read)
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+form.addEventListener('submit', logSubmit);
